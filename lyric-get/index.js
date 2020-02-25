5347d6e95fb89002033a684d31607f5919fac1d0
Oliver Rodas <oliver.rodas@duke.edu>
2020-02-24 19:20:09 -0500

    Trying URL

--- lyric-get/index.js
+++ lyric-get/index.js
@@ -10,7 +10,7 @@ module.exports = {
 
 		url = 'https://www.azlyrics.com/lyrics/' + artist + '/' + song + '.html';
 
-
+		callback(null, url)
 		request(url, function(error, response, html) {
 	        if(error)
 	        {
