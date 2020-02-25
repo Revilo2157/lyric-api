2558cb1acadc19a9dbd20553422f7df10658f86f
Oliver Rodas <oliver.rodas@duke.edu>
2020-02-25 00:31:33 -0500

    Genius

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ router.route('/find/:artist/:song')
 		        
 		        var $ = cheerio.load(html);
 		        $('script').remove();
-		        var lyrics = ($(".lyrics").children().first().next().html());
+		        var lyrics = ($(".lyrics").html());
 		        console.log(lyrics);
 				/**
 				 * Override default underscore escape map
